674432db1b724459d86da3b032b076ef7e529216
Vijay Jaisankar <vijay.jaisankar@iiitb.org>
2021-06-08 05:01:54 +0530

    Create keys.py

---
+++ keys.py
@@ -0,0 +1,18 @@
+# Discord Bot
+
+bot_token = "there's"
+client_secret = "no"
+client_id = "way"
+
+
+# Translation API
+
+swedish_language_code = "sv"
+english_language_code = "en"
+
+
+# Unsplash API
+
+access_key = "these"
+secret_key = "get"
+redirect_uri = "leaked"
